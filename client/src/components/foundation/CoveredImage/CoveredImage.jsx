765c192e03853d3698650cb96176bf48fe338d1b
Karibash <y.karibayashi@gmail.com>
2021-12-06 00:58:50 +0900

    Make the images load lazily

--- client/src/components/foundation/CoveredImage/CoveredImage.jsx
+++ client/src/components/foundation/CoveredImage/CoveredImage.jsx
@@ -6,18 +6,20 @@ import React from 'react';
  * @property {string} className
  * @property {string} src
  * @property {string} alt
+ * @property {string} loading
  */
 
 /**
  * アスペクト比を維持したまま、要素のコンテンツボックス全体を埋めるように画像を拡大縮小します
  * @type {React.VFC<Props>}
  */
-const CoveredImage = ({ className, src, alt }) => {
+const CoveredImage = ({ className, src, alt, loading = 'lazy' }) => {
   return (
     <img
       className={classNames(className, 'w-full h-full object-cover overflow-hidden')}
       src={src}
       alt={alt}
+      loading={loading}
     />
   );
 };
