3c056e50dd40ce0a4215584e5b4b9b0ba8e5c67d
Karibash <y.karibayashi@gmail.com>
2021-12-20 00:47:24 +0900

    Make the code split using loadable-components

--- client/src/containers/AppContainer/AppContainer.jsx
+++ client/src/containers/AppContainer/AppContainer.jsx
@@ -1,19 +1,19 @@
-import React, { lazy } from 'react';
+import React from 'react';
 import { Helmet } from 'react-helmet';
-import { Router } from 'preact-router'
+import { Router } from 'preact-router';
+import loadable from '@loadable/component';
 
 import { AppPage } from '../../components/application/AppPage';
-import { Route } from '../../components/foundation/Route';
 import { useFetch } from '../../hooks/use_fetch';
 import { fetchJSON } from '../../utils/fetchers';
 import { AuthModalContainer } from '../AuthModalContainer';
 import { NewPostModalContainer } from '../NewPostModalContainer';
 
-const TimelineContainer = lazy(() => import('../TimelineContainer').then(module => ({ default: module.TimelineContainer })));
-const UserProfileContainer = lazy(() => import('../UserProfileContainer').then(module => ({ default: module.UserProfileContainer })));
-const PostContainer = lazy(() => import('../PostContainer').then(module => ({ default: module.PostContainer })));
-const TermContainer = lazy(() => import('../TermContainer').then(module => ({ default: module.TermContainer })));
-const NotFoundContainer = lazy(() => import('../NotFoundContainer').then(module => ({ default: module.NotFoundContainer })));
+const TimelineContainer = loadable(() => import('../TimelineContainer').then(module => ({ default: module.TimelineContainer })));
+const UserProfileContainer = loadable(() => import('../UserProfileContainer').then(module => ({ default: module.UserProfileContainer })));
+const PostContainer = loadable(() => import('../PostContainer').then(module => ({ default: module.PostContainer })));
+const TermContainer = loadable(() => import('../TermContainer').then(module => ({ default: module.TermContainer })));
+const NotFoundContainer = loadable(() => import('../NotFoundContainer').then(module => ({ default: module.NotFoundContainer })));
 
 /** @type {React.VFC} */
 const AppContainer = () => {
@@ -39,11 +39,11 @@ const AppContainer = () => {
     <>
       <AppPage activeUser={activeUser}>
         <Router onChange={onChangeRoute}>
-          <Route component={TimelineContainer} path="/" />
-          <Route component={UserProfileContainer} path="/users/:username" />
-          <Route component={PostContainer} path="/posts/:postId" />
-          <Route component={TermContainer} path="/terms" />
-          <Route component={NotFoundContainer} path="*" />
+          <TimelineContainer path="/" />
+          <UserProfileContainer path="/users/:username" />
+          <PostContainer path="/posts/:postId" />
+          <TermContainer path="/terms" />
+          <NotFoundContainer path="*" />
         </Router>
       </AppPage>
       <AuthModalContainer onUpdateActiveUser={setActiveUser} />
